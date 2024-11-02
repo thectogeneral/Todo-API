21b0a3ade5782b58a168321a1ac46a2a03812349
thectogeneral <salemolorundare@gmail.com>
2024-11-02 04:19:41 +0100

    delete todo item

--- Program.cs
+++ Program.cs
@@ -60,5 +60,16 @@ app.MapPut("/todoitems/{id}", async (int id, Todo inputTodo, TodoDb db) =>
     return Results.NoContent();
 });
 
+app.MapDelete("/todoitems/{id}", async (int id, TodoDb db) =>
+{
+    if (await db.Todos.FindAsync(id) is Todo todo)
+    {
+        db.Todo.Remove(todo);
+        await db.SaveChangesAsync();
+        return Results.NoContent();
+    }
+
+    Results.NotFound();
+});
 
 app.Run();
