cf2fddd46c3ad9c99e4842405f39172ba03b784e
thectogeneral <salemolorundare@gmail.com>
2024-11-02 04:06:55 +0100

    post todo item

--- Program.cs
+++ Program.cs
@@ -38,13 +38,13 @@ app.MapGet("/todoitems/{id}", async (int id, TodoDb db) =>
     await db.Todos.FindAsync(id)
     is Todo todo ? Results.Ok(todo) : Results.NotFound());
 
-app.MapPost("/todoitems", async (Todo todo, TodoDb) =>
+app.MapPost("/todoitems", async (Todo todo, TodoDb db) =>
 {
     db.Todos.Add(todo);
     await db.SaveChangesAsync();
 
     return Results.Created($"/todoitems/{todo.Id}", todo);
-})
+});
 
 
 app.Run();
--- obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+290fe5bf838a8052d35110ffdebfe512fd9902c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+feca36639dd4bb8d3ffa383dce0dbed8a7cf2a1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
