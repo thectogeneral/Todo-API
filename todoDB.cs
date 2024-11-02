290fe5bf838a8052d35110ffdebfe512fd9902c3
thectogeneral <salemolorundare@gmail.com>
2024-11-02 03:44:45 +0100

    Setup Todo Db context

---
+++ todoDB.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+
+class TodoDb : DbContext
+{
+    public TodoDb(DbContextOptions<TodoDb> options) : base(options)
+    {
+
+    }
+    public DbSet<Todo> Todos => Set<Todo>();
+}
